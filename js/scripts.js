e6217bb42c8c8939acae5e679216a516d1776e6e
SantiPerGo <45252268+SantiPerGo@users.noreply.github.com>
2021-05-13 14:09:10 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -163,8 +163,15 @@
                         '<div class="mfp-iframe-scaler" >'+
                         '<div class="mfp-close"></div>'+
                         '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                        '</div></div>'
-            }					
+                        '</div></div>',
+                patterns: {
+                            youtube: {
+                               index: 'youtube.com/', 
+                               id: 'v=', 
+                               src: 'https://www.youtube.com/embed/%id%?autoplay=1' 
+                           }
+                }
+            },			
           });
 		  
          
