e768b7fa9a8305d0ccef3b656dcd6d96a447a917
SantiPerGo <45252268+SantiPerGo@users.noreply.github.com>
2021-07-26 21:45:22 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -28,21 +28,36 @@
  
 
 */
-
+particlesJS("particles-js", 
+    {"particles":{"number":{"value":50,"density":
+    {"enable":true,"value_area":1000}},"color":{"value":"#ffffff"},
+    "shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},
+    "polygon":{"nb_sides":5},"image":{"src":"img/github.svg",
+    "width":100,"height":100}},"opacity":{"value":0.5,"random":false,
+    "anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},
+    "size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,
+    "size_min":0.1,"sync":false}},"line_linked":
+    {"enable":true,"distance":150,"color":"#ffffff","opacity":1,"width":1},
+    "move":{"enable":true,"speed":6,"direction":"none","random":false,
+    "straight":false,"out_mode":"out","bounce":false,"attract":
+    {"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":
+    {"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},
+    "onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":
+    {"distance":400,"line_linked":{"opacity":1}},"bubble":
+    {"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},
+    "repulse":{"distance":150,"duration":0.4},"push":{"particles_nb":4},
+    "remove":{"particles_nb":2}}},"retina_detect":true});
 
 (function ($) {
     'use strict';
 
     jQuery(document).ready(function () {
 
-        
        /* Preloader */
 		
         $(window).load(function () {
             $('.preloader').delay(800).fadeOut('slow');
         });
-		
-		
 		
        /* Smooth Scroll */
 
@@ -143,7 +158,6 @@
                '<div class="mfp-img"></div>'+
                '<div class="mfp-bottom-bar portfolio_title">'+
                '<div class="mfp-title"></div>'+
-               '<div class="mfp-counter"></div>'+
                '</div>'+
                '</div>',
 
