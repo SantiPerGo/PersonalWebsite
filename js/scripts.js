742df142735aea3e77b7ed8e0c18940788d2fe27
Santi PerGo <assasinsky@hotmail.com>
2022-08-09 16:07:49 -0500

    Actualizando Sitio Web Completo

--- js/scripts.js
+++ js/scripts.js
@@ -30,14 +30,14 @@
 */
 particlesJS("particles-js", 
     {"particles":{"number":{"value":50,"density":
-    {"enable":true,"value_area":1000}},"color":{"value":"#ffffff"},
-    "shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},
+    {"enable":true,"value_area":1000}},"color":{"value":"#000"},
+    "shape":{"type":"circle","stroke":{"width":0,"color":"#000"},
     "polygon":{"nb_sides":5},"image":{"src":"img/github.svg",
     "width":100,"height":100}},"opacity":{"value":0.5,"random":false,
     "anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},
     "size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,
     "size_min":0.1,"sync":false}},"line_linked":
-    {"enable":true,"distance":150,"color":"#ffffff","opacity":1,"width":1},
+    {"enable":true,"distance":150,"color":"#000","opacity":1,"width":1},
     "move":{"enable":true,"speed":6,"direction":"none","random":false,
     "straight":false,"out_mode":"out","bounce":false,"attract":
     {"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":
